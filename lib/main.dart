4358cf0a20bbb795936bc23e9a1b499da88d7376
AkashPattanaik <akashpattnaik.github@gmail.com>
2021-09-29 20:22:02 +0530

    format

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,17 @@ import 'pages/home_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyApp())
+      home: MyApp(),
+    ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  // This widget is the root of the application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
